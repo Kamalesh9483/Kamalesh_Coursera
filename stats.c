2d6eb5e9ca2fef4f3820a29c363358ba9489d2a7
Kamalesh <kamalesh9483@gmail.com>
2021-04-07 21:53:24 +0530

    Adding code outline and documentation for C1M1 Assessment

--- stats.c
+++ stats.c
@@ -9,35 +9,178 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file stats.c 
+ * @brief stats.c has necessary codes to evaluate statistical operations like mean,median
+ *        also identify maximum, minimum value of given array & sort array in descending order
+ *        
  *
  * <Add Extended Description Here>
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author Add Kamalesh U
+ * @date 6/4/2021
  *
  */
-
-
 
 #include <stdio.h>
 #include "stats.h"
 
+
 /* Size of the Data Set */
 #define SIZE (40)
 
-void main() {
+void find_mean(uint8_t* input, int size)
+{
+  float mean = 0;
+  for (int i=0; i<size; i++) 
+  {
+    mean = input[i] + mean;
+  }
+  mean = mean/SIZE;
+  printf("\nThe mean is %f\n", mean); 
+
+}
+
+
+void swap(uint8_t* p, uint8_t* q)
+{
+  uint8_t temp;
+  temp = *p;
+  *p = *q;
+  *q = temp;
+}
+
+
+void sort_array(uint8_t* array, int n)
+{
+  int i,j;
+  for (i=0; i<n; i++)
+  {
+    for (j=i+1; j<n; j++)
+    {
+      if (array[i] < array[j])
+      {
+        swap(&array[i], &array[j]);
+      }
+    }
+  }
+}
+
+void find_median(uint8_t* array, int n)
+{
+  // even number
+  if (n %2 == 0)
+  {
+    float median;
+    median = (array[(n-1)/2] + array[n/2])/2;
+    printf("\nThe median is %f\n", median);
+  }
+  // odd nmber
+  else 
+  {
+    printf("\n The median is %f\n",(array[n/2]));
+  }
+}
+
+
+void find_maximum(uint8_t* array, int n)
+{
+  int i, j;
+  float max;
+for (i=0; i<n; i++)
+{
+    if (array[i] > array[j])
+    {
+      max = array[i];
+    }
+    else 
+    {
+      max = array[j];
+    }
+}
+printf("\n The maximum value is %f \n",max);
+}
+
+void find_minimum(uint8_t* array, int n)
+{
+  int i,j;
+  float min;
+  for (i=0; i<n; i++)
+  {
+    if (array[i] < array[j])
+    {
+      min = array[i];
+    }
+    else 
+    {
+      min = array[j];
+    }
+  }
+   printf("\nThe minimum value is %f\n",min);
+}
+
+void print_array(uint8_t* array,int n)
+{
+  int i;
+  for (i=0; i<n; i++)
+  {
+    printf("%d\t",array[i]);
+  }
+}
+
+void print_statistics(uint8_t* array, int n)
+{
+  find_minimum(array, n);
+  find_maximum(array, n);
+  find_mean(array, n);
+  find_median(array,n);
+
+}
+
+int main() 
+{
 
   unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
                               114, 88,   45,  76, 123,  87,  25,  23,
                               200, 122, 150, 90,   92,  87, 177, 244,
                               201,   6,  12,  60,   8,   2,   5,  67,
-                                7,  87, 250, 230,  99,   3, 100,  90};
+                              7,  87, 250, 230,  99,   3, 100,  90};
 
   /* Other Variable Declarations Go Here */
   /* Statistics and Printing Functions Go Here */
+ 
+// Print array
 
+print_array(test, SIZE);
+
+//  // Mean
+//  float mean;
+//  mean = find_mean(test, SIZE);
+
+ // Sorting in descending order
+ sort_array(test , SIZE);
+ for (int i=0; i<SIZE; i++)
+ {
+ printf("\n test[%d] = %d \n",i,test[i]);
+ }
+
+//  // Median
+//  float median;
+//  median = find_median(test, SIZE);
+
+
+// // Maximum
+// float maximum;
+// maximum = find_maximum(test, SIZE);
+
+
+// // Minimum
+// float minimum;
+// minimum = find_minimum(test, SIZE);
+
+// statics
+print_statistics(test, SIZE);
+
+
+return 0;
 }
-
 /* Add other Implementation File Code Here */
--- stats.h
+++ stats.h
@@ -9,32 +9,107 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
- *
+ * @file stats.h
+ * @brief stats.h has the necessary functions to perform statistical operations on at
+ *        given array
  * <Add Extended Description Here>
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author Kamalesh U
+ * @date 6/4/2021
  *
  */
 #ifndef __STATS_H__
 #define __STATS_H__
+#include <stdint.h>
 
 /* Add Your Declarations and Function Comments here */ 
 
 /**
- * @brief <Add Brief Description of Function Here>
+ * @brief Function that evaluates and prints the mean of a given array
  *
- * <Add Extended Description Here>
+ * @param input should be an array of type unsigned 8-bit integers
+ * @param size is the number of elements in the input array
  *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- *
- * @return <Add Return Informaiton here>
+ * @return void
  */
+void find_mean(uint8_t* input, int size);
+
+
+/**
+ * @brief Function that swaps the value of two given arguments
+ *
+ * @param p should be an array of type unsigned 8-bit integer
+ * @param q hould be an array of type unsigned 8-bit integer
+ *
+ * @return void
+ */
+void swap(uint8_t* p, uint8_t* q);
+
+
+/**
+ * @brief Function that sorts elements of array in descending order as per their magnitude
+ *
+ * @param array should be an array of type unsigned 8-bit integer
+ * @param n is the number of elements in the input array
+ *
+ * @return void
+ */
+void sort_array(uint8_t* array, int n);
+
+
+/**
+ * @brief Function that evaluates median of given set of array which is in descending order 
+ *
+ * @param array should be an array of type unsigned 8-bit integer
+ * @param n is the number of elements in the input array
+ *
+ * @return void
+ */
+void find_median(uint8_t* array, int n);
+
+
+/**
+ * @brief Function that identifies maximum value of given set of array 
+ *
+ * @param array should be an array of type unsigned 8-bit integer
+ * @param n is the number of elements in the input array
+ *
+ * @return void
+ */
+void find_maximum(uint8_t* array, int n);
+
+
+/**
+ * @brief Function that identifies minimum value of given set of array 
+ *
+ * @param array should be an array of type unsigned 8-bit integer
+ * @param n is the number of elements in the input array
+ *
+ * @return void
+ */
+void find_minimum(uint8_t* array, int n);
+
+
+/**
+ * @brief Function that prints array to the screen
+ *
+ * @param array should be an array of type unsigned 8-bit integer
+ * @param n is the number of elements in the input array
+ *
+ * @return void
+ */
+void print_array(uint8_t* array,int n);
+
+
+/**
+ * @brief Function that prints minimum, maximum, mean, median of given array of elements
+ *
+ * @param array should be an array of type unsigned 8-bit integer
+ * @param n is the number of elements in the input array
+ *
+ * @return void
+ */
+void print_statistics(uint8_t* array, int n);
 
 
 #endif /* __STATS_H__ */
